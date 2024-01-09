0e7d41f6596faf1a8d74033e72c21f05f9cf2c48
jyjyeon <k-j_y@naver.com>
2024-01-09 14:05:43 +0900

    [240108] 맵+셋

---
+++ 2_맵+셋/19583.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let [start, end, finalEnd] = input.shift().split(" ");
+start = Number(start.replace(":", "")); //개강총회 시작 시간
+end = Number(end.replace(":", "")); //개강총회 끝 시간
+finalEnd = Number(finalEnd.replace(":", "")); //스트리밍 끝 시간
+
+const members = new Set();
+let ans = 0;
+
+for (let i = 0; i < input.length; i++) {
+  let [timeString, name] = input[i].trim().split(" ");
+  let time = Number(timeString.replace(":", "")); //회원이 채팅한 시간
+  if (time <= start && !members.has(name)) {
+    //시작전 출석 확인
+    members.add(name);
+  } else if (end <= time && time <= finalEnd && members.has(name)) {
+    //끝난후 출석 확인
+    members.delete(name);
+    ans++;
+  }
+}
+//출력
+console.log(ans);
