4684d6caee4776a547aea1effdc350eebddb107e
jyjyeon <k-j_y@naver.com>
2024-03-22 00:26:22 +0900

    [240322] DFS + BFS

--- 011_DFS+BFS/1260_복습.js
+++ 011_DFS+BFS/1260_복습.js
@@ -3,10 +3,13 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [N, M, V] = input.shift().split(" ").map(Number);
-let graph = new Array(N + 1);
+let graph = Array(N + 1);
+
 for (let i = 0; i < graph.length; i++) {
   graph[i] = [];
 }
+
+//그래프 생성
 for (let i = 0; i < M; i++) {
   let [from, to] = input[i].split(" ").map(Number);
   graph[from].push(to);
@@ -15,40 +18,40 @@ for (let i = 0; i < M; i++) {
 graph.forEach((e) => {
   e.sort((a, b) => a - b);
 });
-let visited = new Array(N + 1).fill(0);
-let dfs = [];
-let bfs = [];
 
-// DFS
-function dfsSearch(v) {
-  if (visited[v]) return;
+let visited = Array(N + 1).fill(0);
+let dfsResult = [];
+function dfs(v) {
+  if (visited[v]) return; //1. 방문한 노드면 리턴
+  //2. 방문 안했으면 방문 체크
   visited[v] = 1;
-  dfs.push(v);
+  dfsResult.push(v);
+  //3. for문 돌며 방문 체크
   for (let i = 0; i < graph[v].length; i++) {
     let next = graph[v][i];
     if (visited[next] === 0) {
-      dfsSearch(next);
+      //방문 안 한 노드면 탐색시작
+      dfs(next);
     }
   }
 }
 
-//출력
-dfsSearch(V);
-console.log(dfs.join(" "));
-
 visited.fill(0);
 
-// BFS
-function bfsSearch(v) {
-  let queue = [v];
-  //큐가 빌 때까지
+let bfsResult = [];
+function bfs(v) {
+  let queue = [v]; //1. 큐 생성
   while (queue.length) {
+    //2. 큐가 비어있지 않다면 루프 돌기
     let x = queue.shift();
     if (visited[x] === 1) {
+      //3. 방문한 노드면 skip
       continue;
     }
+    //4. 방문 안했으면 방문 체크
     visited[x] = 1;
-    bfs.push(x);
+    bfsResult.push(x);
+    //5. for문 돌면서 인접한 노트 큐에 push
     for (let i = 0; i < graph[x].length; i++) {
       let next = graph[x][i];
       if (visited[next] === 0) {
@@ -58,6 +61,8 @@ function bfsSearch(v) {
   }
 }
 
-// 출력
-bfsSearch(V);
-console.log(bfs.join(" "));
+dfs(V);
+bfs(V);
+
+console.log(dfsResult.join(" "));
+console.log(bfsResult.join(" "));
