17c252768faae481185a7f68fbf03c1c08e810d4
jyjyeon <k-j_y@naver.com>
2024-06-20 16:19:08 +0900

    [240620] DFS + BFS

--- 011_DFS+BFS/1260_복습2.js
+++ 011_DFS+BFS/1260_복습2.js
@@ -16,42 +16,45 @@ for (let i = 0; i < M; i++) {
   graph[to].push(from);
 }
 graph.forEach((e) => {
+  //정점 번호 오름차순으로 방문하기 위해 배열
   e.sort((a, b) => a - b);
 });
 
+//DFS
 let visited = Array(N + 1).fill(0);
 let dfsResult = [];
 function dfs(v) {
   if (visited[v]) return; //1. 방문한 노드면 리턴
-  //2. 방문 안했으면 방문 체크
+  //2. 첫 방문이면 방문 기록
   visited[v] = 1;
   dfsResult.push(v);
-  //3. for문 돌며 방문 체크
+  //3. for문 돌며 녿 ㅡ방문
   for (let i = 0; i < graph[v].length; i++) {
     let next = graph[v][i];
     if (visited[next] === 0) {
-      //방문 안 한 노드면 탐색시작
       dfs(next);
     }
   }
 }
 
-visited.fill(0);
+dfs(V);
+console.log(dfsResult.join(" "));
 
+//BFS
+visited.fill(0);
 let bfsResult = [];
 function bfs(v) {
-  let queue = [v]; //1. 큐 생성
+  let queue = [v]; //0. 큐 생성
   while (queue.length) {
-    //2. 큐가 비어있지 않다면 루프 돌기
     let x = queue.shift();
+    //1. 방문한 노드면 skip
     if (visited[x] === 1) {
-      //3. 방문한 노드면 skip
       continue;
     }
-    //4. 방문 안했으면 방문 체크
+    //2. 첫 방문이면 방문 기록
     visited[x] = 1;
     bfsResult.push(x);
-    //5. for문 돌면서 인접한 노트 큐에 push
+    //3. for문 돌며 노드 방문
     for (let i = 0; i < graph[x].length; i++) {
       let next = graph[x][i];
       if (visited[next] === 0) {
@@ -61,12 +64,5 @@ function bfs(v) {
   }
 }
 
-/**
- * 여기에서 한번에 출력하면 안됨! (현재: visited 초기화 => dfs계산 => bfs계산)
- * dfs 계산 => visited 초기화 => bfs 계산 : 이렇게 되어야!!
- */
-dfs(V);
 bfs(V);
-
-console.log(dfsResult.join(" "));
 console.log(bfsResult.join(" "));
